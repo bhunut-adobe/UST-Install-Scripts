faebba47cbd334d12c7455bf19235f8b61ddd0d7
janssenda <janssenda@gmail.com>
2018-03-26 14:31:38 -0500

    Portableize

--- UST_io_certgen.ps1
+++ UST_io_certgen.ps1
@@ -12,7 +12,7 @@ $expirationDay = ($expirationDate - (Get-Date)).Days
 
 $USTFolder = "..\..\"
 $OpenSSL = "openssl.exe"
-$OpenSSLConfig = ".\openssl.cnf"
+$OpenSSLConfig = "openssl.cnf"
 
 if(Test-Path $OpenSSL){
     $argslist = @("/c $OpenSSL",
@@ -30,10 +30,10 @@ if(Test-Path $OpenSSL){
     if($process.ExitCode -eq 0){
         Write-Host "Completed - Certificate located in $USTFolder."
         Pause
-        try {
-            # Will not work without GUI
-            & explorer.exe "https://www.adobe.io/console"
-        } catch {}
+#        try {
+#            # Will not work without GUI
+#            & explorer.exe "https://www.adobe.io/console"
+#        } catch {}
     }else{
         Write-Error "Error Generating Certificate"
     }
